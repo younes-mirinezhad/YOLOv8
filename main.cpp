37b4e7be0632a4e78f5d2eb54864af5918f09d66
Younes Mirinezhad <younes.mirinezhad@gmail.com>
2023-11-11 00:55:01 +0330

    Load onnx model using opencv

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
+#include "segmentor.h"
+#include "segmentor_opencv_dnn.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QString modelPath = "/media/chiko/HDD_1/Work/Training_Scripts/YOLOv8/yolov8n-seg.onnx";
+
+    Segmentor *segmentor{nullptr};
+    segmentor = new Segmentor_OpenCV_DNN;
+
+    auto modelStatus = segmentor->LoadModel(modelPath);
 
     return a.exec();
 }
---
+++ segmentor_opencv_dnn.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "segmentor.h"
+#include <opencv2/opencv.hpp>
+
+struct MaskParams {
+    int netWidth = 640;
+    int netHeight = 640;
+    float maskThreshold = 0.5;
+    cv::Size srcImgShape;
+    cv::Vec4d params;
+};
+
+class Segmentor_OpenCV_DNN : public Segmentor
+{
+public:
+    explicit Segmentor_OpenCV_DNN(QObject *parent = nullptr);
+
+    bool LoadModel(QString& modelPath) override;
+    virtual BatchSegmentedObject Run(MatVector& srcImgList) override;
+
+private:
+    cv::dnn::Net model;
+};
