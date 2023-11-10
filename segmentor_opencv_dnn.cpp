d72acd635709f59d91e79714fb5877f216edbc53
Younes Mirinezhad <younes.mirinezhad@gmail.com>
2023-11-11 01:50:14 +0330

    make blob from input images

--- segmentor_opencv_dnn.cpp
+++ segmentor_opencv_dnn.cpp
@@ -58,5 +58,72 @@ bool Segmentor_OpenCV_DNN::LoadModel(QString &modelPath)
 
 BatchSegmentedObject Segmentor_OpenCV_DNN::Run(MatVector &srcImgList)
 {
-    return {};
+    qDebug() << Q_FUNC_INFO;
+
+    // TODO: just work with bachNumber=1
+    if(_batchSize > 1) {
+        qDebug() <<"This class just work with batchNumber=1";
+        return {};
+    }
+
+    BatchSegmentedObject batchOutput;
+
+    auto srcImg = srcImgList[0];
+    cv::Mat netInputImg;
+    cv::Vec4d params;
+    LetterBox(srcImg, netInputImg, params, _inputSize);
+
+    cv::Mat blob;
+    cv::dnn::blobFromImage(netInputImg, blob, 1 / 255.0, _inputSize, cv::Scalar(0, 0, 0), true, false);
+
+    return batchOutput;
+}
+
+void Segmentor_OpenCV_DNN::LetterBox(const cv::Mat &image, cv::Mat &outImage, cv::Vec4d &params, const cv::Size &newShape, bool autoShape, bool scaleFill, bool scaleUp, int stride, const cv::Scalar &color)
+{
+    cv::Size shape = image.size();
+    float r = std::min((float)newShape.height / (float)shape.height,
+                       (float)newShape.width / (float)shape.width);
+    if (!scaleUp)
+        r = std::min(r, 1.0f);
+
+    float ratio[2]{ r, r };
+    int new_un_pad[2] = { (int)std::round((float)shape.width * r),(int)std::round((float)shape.height * r) };
+
+    auto dw = (float)(newShape.width - new_un_pad[0]);
+    auto dh = (float)(newShape.height - new_un_pad[1]);
+
+    if (autoShape) {
+        dw = (float)((int)dw % stride);
+        dh = (float)((int)dh % stride);
+    }
+    else if (scaleFill) {
+        dw = 0.0f;
+        dh = 0.0f;
+        new_un_pad[0] = newShape.width;
+        new_un_pad[1] = newShape.height;
+        ratio[0] = (float)newShape.width / (float)shape.width;
+        ratio[1] = (float)newShape.height / (float)shape.height;
+    }
+
+    dw /= 2.0f;
+    dh /= 2.0f;
+
+    if (shape.width != new_un_pad[0] && shape.height != new_un_pad[1]) {
+        cv::resize(image, outImage, cv::Size(new_un_pad[0], new_un_pad[1]));
+    }
+    else {
+        outImage = image.clone();
+    }
+
+    int top = int(std::round(dh - 0.1f));
+    int bottom = int(std::round(dh + 0.1f));
+    int left = int(std::round(dw - 0.1f));
+    int right = int(std::round(dw + 0.1f));
+    params[0] = ratio[0];
+    params[1] = ratio[1];
+    params[2] = left;
+    params[3] = top;
+
+    cv::copyMakeBorder(outImage, outImage, top, bottom, left, right, cv::BORDER_CONSTANT, color);
 }
--- segmentor_opencv_dnn.h
+++ segmentor_opencv_dnn.h
@@ -20,4 +20,14 @@ public:
 
 private:
     cv::dnn::Net model;
+
+    void LetterBox(const cv::Mat& image,
+                   cv::Mat& outImage,
+                   cv::Vec4d& params,
+                   const cv::Size& newShape = cv::Size(640, 640),
+                   bool autoShape = false,
+                   bool scaleFill = false,
+                   bool scaleUp = true,
+                   int stride = 32,
+                   const cv::Scalar& color = cv::Scalar(114, 114, 114));
 };
