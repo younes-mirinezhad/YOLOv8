da167e4db543cf0ff87059287c81a0968d8c96fe
Younes Mirinezhad <younes.mirinezhad@gmail.com>
2023-11-11 00:41:39 +0330

    Set sermentors base functions

--- segmentor.cpp
+++ segmentor.cpp
@@ -1,3 +1,24 @@
 #include "segmentor.h"
 
 Segmentor::Segmentor(QObject *parent) : QObject{parent} {}
+
+void Segmentor::setClassNames(std::vector<std::string> newClassNamesList)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    _classNamesList = newClassNamesList;
+}
+
+void Segmentor::setBatchSize(int newBatch)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    _batchSize = newBatch;
+}
+
+void Segmentor::setInputSize(cv::Size newInputSize)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    _inputSize = newInputSize;
+}
--- segmentor.h
+++ segmentor.h
@@ -1,12 +1,33 @@
 #pragma once
-
 #include <QObject>
+#include "opencv2/core/mat.hpp"
+#include <QDebug>
+
+using MatVector = std::vector<cv::Mat>;
+struct SegmentedObject {
+    int classID;
+    float confidence;
+    cv::Rect box;
+    cv::Mat boxMask;
+    std::vector<std::vector<cv::Point>> maskContoursList;
+};
+using ImagesSegmentedObject = std::vector<SegmentedObject>;
+using BatchSegmentedObject = std::vector<ImagesSegmentedObject>;
 
 class Segmentor : public QObject
 {
 public:
     explicit Segmentor(QObject *parent = nullptr);
 
-signals:
+    virtual bool LoadModel(QString& modelPath) = 0;
+    virtual BatchSegmentedObject Run(MatVector& srcImgList) = 0;
 
+    std::vector<std::string> _classNamesList;
+    void setClassNames(std::vector<std::string> newClassNamesList);
+
+    int _batchSize = 1;
+    void setBatchSize(int newBatch);
+
+    cv::Size _inputSize = cv::Size(640, 640);
+    void setInputSize(cv::Size newInputSize);
 };
